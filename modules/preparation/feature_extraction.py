a37ee2969d7b939ada182208fd19b8dfda814538
Arno Alexander <arno2alexander@gmail.com>
2019-06-03 20:45:04 +0700

    Add mean features

--- modules/preparation/feature_extraction.py
+++ modules/preparation/feature_extraction.py
@@ -8,17 +8,39 @@ class FeatureExtraction:
     # HELPER METHODS
 
     @classmethod
-    def get_booking_id(cls, dataframe):
-        return dataframe[common.Feature.FEAT_booking_id].values[0]
+    def get_element(cls, array, at):
+        return array[at]
+
+    @classmethod
+    def mean(cls, array):
+        return np.mean(array)
 
     # MAIN METHODS
 
     @classmethod
     def switch(cls, dataframe, feature):  # TODO add more extraction by feature
         if feature == common.Feature.FEAT_booking_id:
-            return cls.get_booking_id(dataframe)
+            return cls.get_element(dataframe[feature].values, 0)
+        elif feature == common.Feature.FEAT_mean_accuracy:
+            return cls.mean(dataframe[common.Feature.FEAT_accuracy].values)
+        elif feature == common.Feature.FEAT_mean_bearing:
+            return cls.mean(dataframe[common.Feature.FEAT_bearing].values)
+        elif feature == common.Feature.FEAT_mean_acceleration_x:
+            return cls.mean(dataframe[common.Feature.FEAT_acceleration_x].values)
+        elif feature == common.Feature.FEAT_mean_acceleration_y:
+            return cls.mean(dataframe[common.Feature.FEAT_acceleration_y].values)
+        elif feature == common.Feature.FEAT_mean_acceleration_z:
+            return cls.mean(dataframe[common.Feature.FEAT_acceleration_z].values)
+        elif feature == common.Feature.FEAT_mean_gyro_x:
+            return cls.mean(dataframe[common.Feature.FEAT_gyro_x].values)
+        elif feature == common.Feature.FEAT_mean_gyro_y:
+            return cls.mean(dataframe[common.Feature.FEAT_gyro_y].values)
+        elif feature == common.Feature.FEAT_mean_gyro_z:
+            return cls.mean(dataframe[common.Feature.FEAT_gyro_z].values)
+        elif feature == common.Feature.FEAT_mean_speed:
+            return cls.mean(dataframe[common.Feature.FEAT_speed].values)
         else:
-            return -1
+            return np.nan
 
     @classmethod
     def run(cls, dataframe, features):
