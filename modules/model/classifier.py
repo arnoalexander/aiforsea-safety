e1a8e8bd2e0e947d34d7189a6d28bde7dadf94d5
Arno Alexander <arno2alexander@gmail.com>
2019-06-04 19:09:22 +0700

    Add classifier

---
+++ modules/model/classifier.py
@@ -0,0 +1,20 @@
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+
+
+class Classifier(BaseEstimator, ClassifierMixin):
+
+    def __init__(self):
+        self.model = LogisticRegression()
+
+    def fit(self, X, y, sample_weight=None):
+        self.model.fit(X, y, sample_weight=sample_weight)
+
+    def predict(self, X):
+        probabilities = self.model.predict_proba(X)
+        return np.array([probability[-1] for probability in probabilities])
+
+    def score(self, X, y, sample_weight=None):
+        return roc_auc_score(y, self.predict(X), sample_weight=sample_weight)
